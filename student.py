738c03ffef55176363c8861f920526dc205f8261
Nong Van Quang <quang203262@gmail.com>
2026-05-15 09:03:57 +0700

    Thêm chức năng thêm sinh viên

--- student.py
+++ student.py
@@ -1,8 +1,29 @@
 from utils import students
 
 def add_student():
-    print("--- Chức năng: Thêm sinh viên ---")
-    # Thành viên 1 sẽ code ở đây
+    print("\n--- CHỨC NĂNG: THÊM SINH VIÊN ---")
+    
+    # Nhập dữ liệu từ bàn phím
+    id_sv = input("Nhập mã số sinh viên: ")
+    
+    # Kiểm tra trùng mã số sinh viên (bổ trợ thêm cho logic)
+    for s in students:
+        if s['id'] == id_sv:
+            print("Lỗi: Mã sinh viên này đã tồn tại!")
+            return
+
+    name = input("Nhập tên sinh viên: ")
+    major = input("Nhập ngành học: ")
+
+    # Tạo dictionary mới và thêm vào danh sách 
+    new_student = {
+        "id": id_sv,
+        "name": name,
+        "major": major
+    }
+    
+    students.append(new_student)
+    print(f"Chúc mừng! Đã thêm sinh viên {name} thành công.")
 
 def show_students():
     print("--- Chức năng: Hiển thị danh sách ---")
