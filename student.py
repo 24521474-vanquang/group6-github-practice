0bcb97deacf580b7d66101d837f4c8861042ac3f
Nong Van Quang <quang203262@gmail.com>
2026-05-15 09:40:35 +0700

    Thêm chức năng sửa thông tin

--- student.py
+++ student.py
@@ -25,6 +25,31 @@ def add_student():
     students.append(new_student)
     print(f"Chúc mừng! Đã thêm sinh viên {name} thành công.")
 
+def update_student():
+    print("\n--- CHỨC NĂNG: SỬA THÔNG TIN SINH VIÊN ---")
+    id_sv = input("Nhập mã số sinh viên cần sửa: ")
+    
+    # Tìm kiếm sinh viên trong danh sách
+    for s in students:
+        if s['id'] == id_sv:
+            print(f"Đã tìm thấy sinh viên: {s['name']} (Ngành: {s['major']})")
+            
+            # Nhập thông tin mới
+            new_name = input("Nhập tên mới (để trống nếu không đổi): ")
+            new_major = input("Nhập ngành mới (để trống nếu không đổi): ")
+            
+            # Cập nhật nếu người dùng có nhập nội dung mới
+            if new_name:
+                s['name'] = new_name
+            if new_major:
+                s['major'] = new_major
+                
+            print("Cập nhật thông tin thành công!")
+            return
+
+    # Nếu chạy hết vòng lặp mà không thấy ID
+    print(f"Không tìm thấy sinh viên có mã {id_sv}!")
+
 def show_students():
     print("--- Chức năng: Hiển thị danh sách ---")
     # Thành viên 2 sẽ code ở đây
